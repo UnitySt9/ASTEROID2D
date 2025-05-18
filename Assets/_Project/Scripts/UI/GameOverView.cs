5d598a6de462a2110eac324fa06c523450d0fd20
ZaurKushiev <zkm319011@gmail.com>
2025-05-18 21:05:20 +0400

    feat: added DoTween animation

--- Assets/_Project/Scripts/UI/GameOverView.cs
+++ Assets/_Project/Scripts/UI/GameOverView.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 namespace _Project.Scripts
 {
@@ -9,6 +10,18 @@ namespace _Project.Scripts
         [SerializeField] private TextMeshProUGUI _endScore;
         [SerializeField] private TextMeshProUGUI _highScore;
         [SerializeField] private GameObject _newRecordLabel;
+        private CanvasGroup _canvasGroup;
+
+        private void Awake()
+        {
+            _canvasGroup = _gameOverPanel.GetComponent<CanvasGroup>();
+            if (_canvasGroup == null)
+            {
+                _canvasGroup = _gameOverPanel.AddComponent<CanvasGroup>();
+            }
+            _canvasGroup.alpha = 0f;
+            _gameOverPanel.SetActive(false);
+        }
 
         public void ShowPanel(int score, int highScore, bool isNewRecord)
         {
@@ -16,11 +29,15 @@ namespace _Project.Scripts
             _endScore.text = $"GAME OVER. SCORE: {score}";
             _highScore.text = $"BEST SCORE: {highScore}";
             _newRecordLabel.SetActive(isNewRecord);
+            
+            _canvasGroup.DOFade(1f, 2f).SetEase(Ease.OutQuad);
+            _gameOverPanel.transform.localScale = Vector3.one * 7f;
+            _gameOverPanel.transform.DOScale(new Vector3(0.8f, 0.8f, 0.8f), 1f).SetEase(Ease.OutBack);
         }
         
         public void HidePanel()
         {
-            _gameOverPanel.SetActive(false);
+            _canvasGroup.DOFade(0f, 0.3f).OnComplete(() => _gameOverPanel.SetActive(false));
         }
     }
 }
