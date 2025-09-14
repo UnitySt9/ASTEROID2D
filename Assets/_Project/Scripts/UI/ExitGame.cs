9fd17a0f342b7be5c305aaa66668a0a42d477a1a
ZaurKushiev <zkm319011@gmail.com>
2025-09-14 13:13:07 +0300

    feat: Final Step

---
+++ Assets/_Project/Scripts/UI/ExitGame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class ExitGame : MonoBehaviour
+    {
+        public void ExitApplication()
+        {
+        Application.Quit();
+        }
+    }
+}
